9cdc5b251f793b600d979b93b46326b49cf72c58
Happi3r <dev.happi3r@gmail.com>
2023-04-18 20:48:16 +0900

    tttt

--- lib/page/teacher_page.dart
+++ lib/page/teacher_page.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:school_nurse_ofiice/util/firebase.dart';
 import 'package:table_calendar/table_calendar.dart';
+
 class TeacherPage extends StatefulWidget {
   const TeacherPage({Key? key}) : super(key: key);
 
@@ -12,11 +15,23 @@ class TeacherPage extends StatefulWidget {
 class _TeacherPageState extends State<TeacherPage> {
   @override
   Widget build(BuildContext context) {
+    final now = DateTime.now();
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
-            child: TableCalendar,
+            child: StreamBuilder<Object>(
+                stream: Firebase.queryDiaryfromDate(
+                  isGtEq: Timestamp.fromDate(DateTime(now.year, now.month)),
+                  isLt: Timestamp.fromDate(DateTime(now.year, now.month + 1)),
+                ).snapshots(),
+                builder: (context, snapshot) {
+                  return TableCalendar(
+                    firstDay: DateTime.utc(2023, 1, 16),
+                    lastDay: DateTime(now.year + 10),
+                    focusedDay: now,
+                  );
+                }),
           ),
         ),
       ),
